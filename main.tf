d470ed2d1c931448c8edfd6b27ad7860e92b0bbf
kasou-sekai <3398588562@qq.com>
2026-03-17 17:21:02 +1000

    finish

---
+++ main.tf
@@ -0,0 +1,63 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 6.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+    shared_credentials_files = ["./credentials"]
+    default_tags {
+        tags = {
+            Environment = "Dev"
+            Course = "CSSE6400"
+            StudentID = "49842478"
+        }
+    }
+}
+
+resource "aws_security_group" "hextris-server" {
+    name = "hextris-sg"
+    description = "Allow HTTP and SSH access"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_instance" "hextris-server" {
+    ami = "ami-0c421724a94bba6d6"
+    instance_type = "t2.micro"
+    key_name = "vockey"
+    security_groups = [aws_security_group.hextris-server.name]
+    user_data = file("./serve-hextris.sh")
+    
+
+    tags = {
+        Name = "hextris"
+    }
+}
+
+output "hextris-url" {
+    value = aws_instance.hextris-server.public_ip
+}
